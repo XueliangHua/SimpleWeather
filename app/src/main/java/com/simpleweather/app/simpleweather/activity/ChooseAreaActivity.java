b69002b46c97f85f2cbd610250c02e1f80ed8c70
XueliangHua <xueliang2832@gmail.com>
2016-09-02 14:35:19 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/simpleweather/app/simpleweather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/simpleweather/app/simpleweather/activity/ChooseAreaActivity.java
@@ -68,12 +68,18 @@ public class ChooseAreaActivity extends Activity {
      */
     private int currentLevel;
 
-    @Override
+    /**
+     * 是否从WeatherActivity 中跳转过来
+     */
+    private boolean isFromWeatherActivity;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if (prefs.getBoolean("city_selected", false)) {
+        //已经选择了城市并不是从WeatherActivity跳转过来，才会直接跳转到WeatherActivity
+        if (prefs.getBoolean("city_selected", false) && !isFromWeatherActivity) {
             Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
             finish();
@@ -257,6 +263,10 @@ public class ChooseAreaActivity extends Activity {
         } else if (currentLevel == LEVEL_CITY) {
             queryProvinces();
         } else {
+            if (isFromWeatherActivity) {
+                Intent intent = new Intent(this, WeatherActivity.class);
+                startActivity(intent);
+            }
             finish();
         }
     }
